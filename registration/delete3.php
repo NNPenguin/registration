5cd155e8a91de37a2bf7726a0ba520246e05d2a1
NNPenguin <112741061+NNPenguin@users.noreply.github.com>
2022-09-22 12:08:11 +0900

    変更
    コメント削除

--- registration/delete3.php
+++ registration/delete3.php
@@ -6,7 +6,6 @@
 </head>
 <body>
 <?php
-//SESSIONのチェックがある場合はREQUEST＿METHODは記載不要
 	if($_SERVER["REQUEST_METHOD"]!=="POST"):
 		exit("直接アクセス禁止");
 	endif;
@@ -15,8 +14,6 @@
 	if(!$link){
 		exit("データベースに接続できません".mysqli_connect_error());
 	}
-//入力ミスチェック
-//issetしてからpregmatchしたほうが安定する
 	if(!(isset($_POST['pass']) && preg_match("/^[a-zA-Z0-9]{6,12}$/",$_POST['pass']))):
 ?>
 	<p>正しく入力してください</p>  
@@ -33,7 +30,7 @@
         mysqli_stmt_bind_result($sql,$pass);
         mysqli_stmt_fetch($sql);
         if($_POST["pass"]===$pass):
-            mysqli_stmt_close($sql);//新しいsql文の前に一度stmtをクローズする。しないと新しいsql文に入れない。
+            mysqli_stmt_close($sql);
             $sql = mysqli_prepare($link,'DELETE FROM `member` WHERE `name`=?');
             mysqli_stmt_bind_param($sql,'s',$_POST['name']);
             mysqli_stmt_execute($sql);
@@ -44,8 +41,6 @@
 	<p>パスワードが違います</p>
 <?php
 			endif;
-//44行目のif文と54～57行目のelseブロックとendifはsessionで行ってる場合不要
-//今回は入力してるので入力ミス確認のため
 		else:
 ?>
 	<p>ユーザーが存在しません</p>
